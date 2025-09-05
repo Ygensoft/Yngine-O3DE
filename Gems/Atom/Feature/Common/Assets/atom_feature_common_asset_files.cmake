o3de/o3de

Move MeshInfo and MaterialInfo from the RayTracingFeatureProcessor to self-contained parts of the MeshFeatureProcessor
Signed-off-by: Karl Haubenwallner <karl.haubenwallner@huawei.com>

Gems/Atom/Feature/Common/Assets/atom_feature_common_asset_files.cmake
@@ -338,6 +338,10 @@ set(FILES
     ShaderLib/Atom/Features/PBR/Surfaces/TransmissionSurfaceData.azsli
     ShaderLib/Atom/Features/Pipeline/Forward/ForwardPassOutput.azsli
     ShaderLib/Atom/Features/Pipeline/Forward/ForwardPassSrg.azsli
+    ShaderLib/Atom/Features/FallbackPBRMaterial/FallbackPBRMaterial.azsli
+    ShaderLib/Atom/Features/FallbackPBRMaterial/FallbackPBRMaterialUtil.azsli
+    ShaderLib/Atom/Features/MeshInfo/MeshInfo.azsli
+    ShaderLib/Atom/Features/MeshInfo/MeshInfoUtil.azsli
     ShaderLib/Atom/Features/PostProcessing/Aces.azsli
     ShaderLib/Atom/Features/PostProcessing/AcesColorSpaceConversion.azsli
     ShaderLib/Atom/Features/PostProcessing/FullscreenPixelInfo.azsli
@@ -352,8 +356,6 @@ set(FILES
     ShaderLib/Atom/Features/PostProcessing/Tonemap.azsli
     ShaderLib/Atom/Features/RayTracing/RayTracingGlobalSrg.azsli
     ShaderLib/Atom/Features/RayTracing/RayTracingIntersectionAttributes.azsli
-    ShaderLib/Atom/Features/RayTracing/RayTracingMaterialSrg.azsli
-    ShaderLib/Atom/Features/RayTracing/RayTracingMaterialUtils.azsli
     ShaderLib/Atom/Features/RayTracing/RayTracingSceneSrg.azsli
     ShaderLib/Atom/Features/RayTracing/RayTracingSceneUtils.azsli
     ShaderLib/Atom/Features/RayTracing/RayTracingSrgs.azsl

Gems/Atom/Feature/Common/Code/Source/Material/FallbackPBRMaterialManager.h
@@ -1,0 +1,80 @@
+/*
+ * Copyright (c) Contributors to the Open 3D Engine Project.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ *
+ */
+
+#pragma once
+
+#include <Atom/Feature/Mesh/MeshFeatureProcessorInterface.h>
+#include <Atom/Feature/Mesh/MeshInfo.h>
+#include <Atom/Feature/Mesh/MeshInfoBus.h>
+#include <Atom/Feature/ReflectionProbe/ReflectionProbeFeatureProcessorInterface.h>
+#include <Atom/Feature/TransformService/TransformServiceFeatureProcessorInterface.h>
+#include <Atom/RPI.Public/Buffer/BufferSystemInterface.h>
+#include <Atom/RPI.Public/Buffer/RingBuffer.h>
+#include <Atom/RPI.Public/Material/Material.h>
+#include <Atom/RPI.Public/Model/Model.h>
+#include <Atom/RPI.Public/Scene.h>
+
+namespace AZ::Render
+{
+    namespace FallbackPBR
+    {
+        class MaterialManager : public MeshInfoNotificationBus::Handler
+        {
+        public:
+            MaterialManager();
+            void Activate(RPI::Scene* scene);
+            void Deactivate();
+            void Update();
+
+            // MeshInfoNotificationBus::Handler overrides
+            void OnCreateMeshInfoEntry(
+                const MeshInfoHandle meshInfoHandle,
+                ModelDataInstanceInterface* modelData,
+                const size_t lodIndex,
+                const size_t lodMeshIndex) override;
+            void OnDeleteMeshInfoEntry(const MeshInfoHandle meshInfoHandle) override;
+
+            // integration with the MeshFeatureProcessor
+            const RHI::Ptr<MaterialEntry>& GetFallbackPBRMaterialEntry(const MeshInfoHandle handle) const;
+            void UpdateFallbackPBRMaterialEntry(const MeshInfoHandle handle, AZStd::function<bool(MaterialEntry*)> updateFunction);
+            const Data::Instance<RPI::Buffer>& GetFallbackPBRMaterialBuffer() const;
+
+            void UpdateReflectionProbes(const TransformServiceFeatureProcessorInterface::ObjectId& objectId, const Aabb& aabbWS);
+
+        private:
+            bool m_isEnabled = false;
+
+            void UpdateFallbackPBRMaterial();
+            void UpdateFallbackPBRMaterialBuffer();
+
+            void ConvertMaterial(RPI::Material* material, MaterialParameters& convertedMaterial);
+
+            // reflection probe
+            struct ReflectionProbe
+            {
+                AZ::Transform m_modelToWorld;
+                AZ::Vector3 m_outerObbHalfLengths;
+                AZ::Vector3 m_innerObbHalfLengths;
+                bool m_useParallaxCorrection = false;
+                float m_exposure = 0.0f;
+
+                Data::Instance<RPI::Image> m_reflectionProbeCubeMap;
+            };
+            AZStd::unordered_map<TransformServiceFeatureProcessorInterface::ObjectId, ReflectionProbe> m_reflectionProbeData;
+
+            RHI::Ptr<MaterialEntry> m_emptyEntry{ nullptr };
+            AZStd::vector<RHI::Ptr<MaterialEntry>> m_materialData;
+            RPI::RingBuffer m_materialDataBuffer;
+            bool m_bufferNeedsUpdate;
+
+            RHI::ShaderInputNameIndex m_fallbackPBRMaterialIndex = "m_fallbackPBRMaterial";
+            RPI::Scene::PrepareSceneSrgEvent::Handler m_updateSceneSrgHandler;
+            ReflectionProbeFeatureProcessorInterface* m_rpfp = nullptr;
+        };
+    } // namespace FallbackPBR
+} // namespace AZ::Render

Ygensoft/Yngine-O3DE

Gems/Atom/Feature/Common/Assets/atom_feature_common_asset_files.cmake
@@ -349,6 +349,13 @@ set(FILES
     ShaderLib/Atom/Features/Pipeline/Forward/ForwardPassSrg.azsli
     ShaderLib/Atom/Features/Pipeline/Forward/ForwardPassVertexData.azsli
     ShaderLib/Atom/Features/Pipeline/Forward/Readme.md
+    ShaderLib/Atom/Features/FallbackPBRMaterial/MaterialInfo.azsli
+    ShaderLib/Atom/Features/FallbackPBRMaterial/MaterialInfoUtil.azsli
+    ShaderLib/Atom/Features/FallbackPBRMaterial/FallbackPBRMaterial_Defines.azsli
+    ShaderLib/Atom/Features/FallbackPBRMaterial/FallbackPBRMaterial.azsli
+    ShaderLib/Atom/Features/FallbackPBRMaterial/FallbackPBRStandardLighting.azsli
+    ShaderLib/Atom/Features/MeshInfo/MeshInfo.azsli
+    ShaderLib/Atom/Features/MeshInfo/MeshInfoUtil.azsli
     ShaderLib/Atom/Features/PostProcessing/Aces.azsli
     ShaderLib/Atom/Features/PostProcessing/AcesColorSpaceConversion.azsli
     ShaderLib/Atom/Features/PostProcessing/FullscreenPixelInfo.azsli
@@ -363,8 +370,6 @@ set(FILES
     ShaderLib/Atom/Features/PostProcessing/Tonemap.azsli
     ShaderLib/Atom/Features/RayTracing/RayTracingGlobalSrg.azsli
     ShaderLib/Atom/Features/RayTracing/RayTracingIntersectionAttributes.azsli
-    ShaderLib/Atom/Features/RayTracing/RayTracingMaterialSrg.azsli
-    ShaderLib/Atom/Features/RayTracing/RayTracingMaterialUtils.azsli
     ShaderLib/Atom/Features/RayTracing/RayTracingSceneSrg.azsli
     ShaderLib/Atom/Features/RayTracing/RayTracingSceneUtils.azsli
     ShaderLib/Atom/Features/RayTracing/RayTracingSrgs.azsl

Gems/Atom/Feature/Common/Code/Source/Material/FallbackPBRMaterialManager.h
@@ -1,0 +1,85 @@
+/*
+ * Copyright (c) Contributors to the Open 3D Engine Project.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ *
+ */
+
+#pragma once
+
+#include <Atom/Feature/Mesh/MeshFeatureProcessorInterface.h>
+#include <Atom/Feature/Mesh/MeshInfo.h>
+#include <Atom/Feature/Mesh/MeshInfoBus.h>
+#include <Atom/Feature/ReflectionProbe/ReflectionProbeFeatureProcessorInterface.h>
+#include <Atom/Feature/TransformService/TransformServiceFeatureProcessorInterface.h>
+#include <Atom/RPI.Public/Buffer/BufferSystemInterface.h>
+#include <Atom/RPI.Public/Buffer/RingBuffer.h>
+#include <Atom/RPI.Public/Material/Material.h>
+#include <Atom/RPI.Public/Model/Model.h>
+#include <Atom/RPI.Public/Scene.h>
+#include <AzCore/std/parallel/mutex.h>
+
+
+namespace AZ::Render
+{
+    namespace FallbackPBR
+    {
+        class MaterialManager : public MeshInfoNotificationBus::Handler
+        {
+        public:
+            MaterialManager();
+            void Activate(RPI::Scene* scene);
+            void Deactivate();
+            void Update();
+
+            // --------------------------------------------------------------------------------------
+            // MeshInfoNotificationBus::Handler overrides
+            void OnAcquireMeshInfoEntry(const MeshInfoHandle meshInfoHandle) override;
+            void OnReleaseMeshInfoEntry(const MeshInfoHandle meshInfoHandle) override;
+            void OnPopulateMeshInfoEntry(
+                const MeshInfoHandle meshInfoHandle,
+                ModelDataInstanceInterface* modelData,
+                const size_t lodIndex,
+                const size_t lodMeshIndex) override;
+            // --------------------------------------------------------------------------------------
+
+            // integration with the MeshFeatureProcessor
+            const RHI::Ptr<MaterialEntry> GetFallbackPBRMaterialEntry(const MeshInfoHandle handle);
+            void UpdateFallbackPBRMaterialEntry(const MeshInfoHandle handle, AZStd::function<bool(MaterialEntry*)> updateFunction);
+            const Data::Instance<RPI::Buffer>& GetFallbackPBRMaterialBuffer() const;
+
+            void UpdateReflectionProbes(const TransformServiceFeatureProcessorInterface::ObjectId& objectId, const Aabb& aabbWS);
+
+        private:
+            bool m_isEnabled = false;
+
+            void UpdateFallbackPBRMaterial();
+            void UpdateFallbackPBRMaterialBuffer();
+
+            void ConvertMaterial(RPI::Material* material, MaterialParameters& convertedMaterial);
+
+            // reflection probe
+            struct ReflectionProbe
+            {
+                AZ::Transform m_modelToWorld;
+                AZ::Vector3 m_outerObbHalfLengths;
+                AZ::Vector3 m_innerObbHalfLengths;
+                bool m_useParallaxCorrection = false;
+                float m_exposure = 0.0f;
+
+                Data::Instance<RPI::Image> m_reflectionProbeCubeMap;
+            };
+            AZStd::unordered_map<TransformServiceFeatureProcessorInterface::ObjectId, ReflectionProbe> m_reflectionProbeData;
+
+            AZStd::mutex m_mutex;
+            AZStd::vector<RHI::Ptr<MaterialEntry>> m_materialData;
+            RPI::RingBuffer m_materialDataBuffer;
+            bool m_bufferNeedsUpdate;
+
+            RHI::ShaderInputNameIndex m_fallbackPBRMaterialIndex = "m_fallbackPBRMaterial";
+            RPI::Scene::PrepareSceneSrgEvent::Handler m_updateSceneSrgHandler;
+            ReflectionProbeFeatureProcessorInterface* m_rpfp = nullptr;
+        };
+    } // namespace FallbackPBR
+} // namespace AZ::Render